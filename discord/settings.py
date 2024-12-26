781cc5d922c2d24ab2e240bf1f37cfdbe698c710
AYUSH KHAIRE <141166916+AYUSHKHAIRE@users.noreply.github.com>
2024-12-26 16:11:09 +0530

    Update settings.py removed sensitive creds

--- discord/settings.py
+++ discord/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3y#iki)me-ut^dk*+ev*d*ulbe$uqka=(p-^(cx&12lo8jv3%1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'daphne',
     'chat',
-    'notify',
     'core',
     'django.contrib.admin',
     'django.contrib.auth',
